e14f868e9e80b9dfa0b6a2e95f9ebf416c825b7b
yhannis4 <yoprudencio@up.edu.ph>
2026-03-07 12:52:19 +0800

    feat: added mock user data

---
+++ src/app/lib/data/user.ts
@@ -0,0 +1,15 @@
+
+
+
+export type User = {
+    id: string
+    name: string
+    email: string
+    //timeCreated , ...
+}                           //temporary user schema
+
+const MOCK_USERS: User[] = [
+    {id: "1", name: "Mahoraga", email:"nahidadapt@mappa.com"},
+    {id: "2", name: "John", email:"johnkaisen@mappa.com"},
+]
+
