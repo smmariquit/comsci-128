1d18da225e96ff3164a42bf4367ef816ca78db48
GiovannApolinar <gnapolinar1@up.edu.ph>
2026-03-08 11:24:52 +0800

    chore: user variables to reflect User entity as defined in ERD

--- src/app/lib/models/user.ts
+++ src/app/lib/models/user.ts
@@ -1,35 +1,35 @@
 //User Model Interface
 
-export type UserRole = 
-  | 'Student' 
-  | 'Dormitory Manager' 
-  | 'Housing Administrator'     
-  | 'System Administrator';               
+export type UserRole =
+	| "Student"
+	| "Dormitory Manager"
+	| "Housing Administrator"
+	| "Landlord"
+	| "System Administrator";
 
-export type AccountStatus = 'Active' | 'Inactive';
+export type AccountStatus = "Active" | "Inactive";
 
 export interface User {
-  // For Audit Trails 
-  userId: string; 
-  
-  // Authentication: must be UP Mail / Google
-  email: string; 
-  
-  // Mandatory profile fields
-  fullName: string;
-  contactNumber: string;   
-  emergencyContact: string; 
-  
-  // Role-Based Access Control
-  role: UserRole;
-  
-  // Status for account deactivation
-  status: AccountStatus;
-  
-  // Management is restricted to assigned dorms
-  assignedDormitories: string[]; 
-  
-  // Timestamps for activity logs 
-  createdAt: Date;
-  updatedAt: Date;
+	// For Audit Trails
+	userId: string;
+
+	// Authentication: must be UP Mail / Google
+	email: string;
+
+	// User Profile fields
+	firstName: string;
+	middleName?: string; // optional
+	lastName: string;
+	permAddress: string;
+	contactNumber: string;
+	contactEmail?: string; // optional
+	sex?: string; // optional
+	birthday?: Date; // optional
+	age?: number; // optional
+
+	// Role-Based Access Control
+	role: UserRole;
+
+	// Status for account deactivation
+	status: AccountStatus;
 }
