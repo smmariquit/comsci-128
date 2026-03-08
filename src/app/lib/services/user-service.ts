8acd01679cfe45baf49cbf67c467efeadd8d49c9
GiovannApolinar <gnapolinar1@up.edu.ph>
2026-03-08 12:26:22 +0800

    feat: user service connects  with user data

--- src/app/lib/services/user-service.ts
+++ src/app/lib/services/user-service.ts
@@ -1,31 +1,16 @@
-// MOCK INTERFACE & DATA FOR FUNCTION TESTING. WILL BE FIXED ONCE OTHER DEPENDENCIES ARE MERGED
-export interface User {
-    id: string;
-    name: string;
-    email: string;
-}
-
-const MOCK_DATA: User[] = [
-    {id: "1", name: "Mahoraga", email: "nahidadapt@mappa.com"}
-];
-// MOCK INTERFACE & DATA FOR FUNCTION TESTING. WILL BE FIXED ONCE OTHER DEPENDENCIES ARE MERGED
+import { findUserById } from "@/lib/data/user";
+import { User } from "../models/user";
 
 // getProfile - INPUT: userId | OUTPUT: user (if found), null/error (if not)
 export const getProfile = async (userId: string): Promise<User | null> => {
-    try {
-        const user = MOCK_DATA.find((u) => u.id === userId);
+	try {
+		const userProfile = await findUserById(userId);
 
-        if (!user) {
-            return null;
-        }
+		if (!userProfile) return null;
 
-        return user;
-    } catch (error) {
-        console.error("Error: ", error);
-        throw new Error("Error");
-    }
-    
-}
-
-// console testing
-getProfile("1").then(user => console.log("User Profile Found: ", user));
+		return userProfile;
+	} catch (error) {
+		console.error("Error: ", error);
+		throw new Error("Error");
+	}
+};
