f43c790c1f232b40080c6484abb60cc774c5b90a
GiovannApolinar <gnapolinar1@up.edu.ph>
2026-03-08 12:14:49 +0800

    chore: mock users reflects user model

--- src/app/lib/data/user.ts
+++ src/app/lib/data/user.ts
@@ -1,26 +1,81 @@
+import { User } from "@/lib/models/user";
 
-
-
-export type User = {
-    id: string
-    name: string
-    email: string
-    //timeCreated , ...
-}                           //temporary user schema
-
+//This serves as the mock 'Database' to test functions
 const MOCK_USERS: User[] = [
-    {id: "1", name: "Mahoraga", email:"nahidadapt@mappa.com"},          //This serves as the mock 'Database' to test functions
-    {id: "2", name: "John", email:"johnkaisen@mappa.com"},
-    {id: "3", name: "Susan", email:"susangrotto@mgmail.com"},
-    {id: "4", name: "Bruce Wayne", email:"alfredbuttler@wayne.com"},
-    {id: "5", name: "Allan", email:"allancruz@gov.ph"},
-]
+	{
+		userId: "1",
+		email: "nahidadapt@mappa.com",
 
-export async function getUserById(userId:string): Promise<User | null>{ //This function takes a USERID of type STRING.
-                                                                        // RETURNS a USER object when found in the DB, otherwise return null.
+		firstName: "Mahoraga",
+		lastName: "Shikigami",
+		permAddress: "Shibuya, Tokyo, Japan",
+		contactNumber: "+81-90-0000-0001",
 
-    //TODO: replace with actual Supabase queries when available
-    
-    const user = MOCK_USERS.find((u) => u.id === userId)
-    return user ?? null
+		role: "Student",
+		status: "Active",
+	},
+	{
+		userId: "2",
+		email: "johnkaisen@mappa.com",
+
+		firstName: "John",
+		lastName: "Kaisen",
+		permAddress: "Shinjuku, Tokyo, Japan",
+		contactNumber: "+81-90-0000-0002",
+		contactEmail: "john.personal@example.com",
+
+		role: "Student",
+		status: "Inactive",
+	},
+	{
+		userId: "3",
+		email: "susangrotto@mgmail.com",
+
+		firstName: "Susan",
+		lastName: "Grotto",
+		permAddress: "Kyoto, Japan",
+		contactNumber: "+81-90-0000-0003",
+		sex: "Female",
+
+		role: "Landlord",
+		status: "Active",
+	},
+	{
+		userId: "4",
+		email: "alfredbuttler@wayne.com",
+
+		firstName: "Bruce",
+		middleName: "Thomas",
+		lastName: "Wayne",
+		permAddress: "Gotham City",
+		contactNumber: "+1-202-555-0104",
+		birthday: new Date("1985-02-19"),
+		age: 40,
+
+		role: "Housing Administrator",
+		status: "Active",
+	},
+	{
+		userId: "5",
+		email: "allancruz@gov.ph",
+
+		firstName: "Allan",
+		lastName: "Cruz",
+		permAddress: "Quezon City, Philippines",
+		contactNumber: "+63-917-000-0005",
+		sex: "Male",
+
+		role: "Dormitory Manager",
+		status: "Active",
+	},
+];
+
+export async function findUserById(userId: string): Promise<User | null> {
+	//This function takes a USERID of type STRING.
+	// RETURNS a USER object when found in the DB, otherwise return null.
+
+	//TODO: replace with actual Supabase queries when available
+
+	const user = MOCK_USERS.find((u) => u.userId === userId);
+	return user ?? null;
 }
