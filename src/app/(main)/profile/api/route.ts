48caa7675cf4134406bb97b21c870f0934e0b7e1
GiovannApolinar <gnapolinar1@up.edu.ph>
2026-03-08 12:29:25 +0800

    feat: profile/api properly connects with user service

--- src/app/(main)/profile/api/route.ts
+++ src/app/(main)/profile/api/route.ts
@@ -1,58 +1,42 @@
 import { NextRequest, NextResponse } from "next/server";
 import { headers } from "next/headers";
-//TODO: import user service
-
-// FOR TESTING ONLY since nasa pr pa ang mock db & getProfile
-// TOBE replaced by import
-const getProfile = async (userId: string) => {
-    const MOCK_USERS = [
-        {id: "1", name: "Mahoraga", email:"nahidadapt@mappa.com"},          //This serves as the mock 'Database' to test functions
-        {id: "2", name: "John", email:"johnkaisen@mappa.com"},
-        {id: "3", name: "Susan", email:"susangrotto@mgmail.com"},
-        {id: "4", name: "Bruce Wayne", email:"alfredbuttler@wayne.com"},
-        {id: "5", name: "Allan", email:"allancruz@gov.ph"},
-    ];
-    
-    const user = MOCK_USERS.find((u) => u.id === userId);
-
-    if (!user) {
-        return null;
-    }
-
-    return user;
-}
+import { getProfile } from "@/app/lib/services/user-service";
 
 // For retrieving profile information of current user
 // Default route for /profile/api
 export async function GET(request: NextRequest) {
-    try{
-        // Check auth
-        const headersList = await headers();
-        const userId = headersList.get("x-user-id"); //must be added by middleware after user auth
+	try {
+		// Check auth
+		const headersList = await headers();
+		const userId = headersList.get("x-user-id"); //must be added by middleware after user auth
 
-        if(!userId){
-            return NextResponse.json({
-                message: "Unauthorized: Invalid Token."
-            },{ status: 401})
-        }
-        // Check request/call user service
-        const user = await getProfile(userId); 
-        
-        // Send Response
-        if (!user) { // User not found
-            return NextResponse.json(
-                { message: 'User not found.' }, 
-                { status: 404 }
-            );
-        }
-        
-        // User found
-        return NextResponse.json(user, { status: 200 });
-    } catch (error: any) {
-        console.error('Error fetching user profile:', error);
-        return NextResponse.json(
-            { message: 'Failed to fetch user.', error: error.message }, 
-            { status: 500 }
-        );
-    }
+		if (!userId) {
+			return NextResponse.json(
+				{
+					message: "Unauthorized: Invalid Token.",
+				},
+				{ status: 401 },
+			);
+		}
+		// Check request/call user service
+		const user = await getProfile(userId);
+
+		// Send Response
+		if (!user) {
+			// User not found
+			return NextResponse.json(
+				{ message: "User not found." },
+				{ status: 404 },
+			);
+		}
+
+		// User found
+		return NextResponse.json(user, { status: 200 });
+	} catch (error: any) {
+		console.error("Error fetching user profile:", error);
+		return NextResponse.json(
+			{ message: "Failed to fetch user.", error: error.message },
+			{ status: 500 },
+		);
+	}
 }
