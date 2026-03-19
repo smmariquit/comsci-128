1f5e2a0f602c9669a34a639e1ed8a1a1ac665501
GiovannApolinar <gnapolinar1@up.edu.ph>
2026-03-19 19:48:00 +0800

    feat: connects read api to supabase

--- src/app/lib/services/user-service.ts
+++ src/app/lib/services/user-service.ts
@@ -1,5 +1,4 @@
-import { findUserById } from "@/data/user";
-import { User } from "@/models/user";
+import { findUserById, User } from "@/data/user";
 
 // getProfile - INPUT: userId | OUTPUT: user (if found), null/error (if not)
 export const getProfile = async (userId: string): Promise<User | null> => {
