4922b7c8123a88bc9edccf23a71b6f4f16283022
JoshuaPagcaliwagan-ui <jopagcaliwagan@up.edu.ph>
2026-03-19 17:48:54 +0000

    feat: added frontend counterpart to show functionality of housing post/get

--- src/app/api/housing/[housingId]/route.ts
+++ src/app/api/housing/[housingId]/route.ts
@@ -4,15 +4,18 @@ import { getHousingById } from '@/app/lib/data/housing';
 // Retrieve a specific dorm's details using the ID from the URL
 export async function GET(
   req: NextRequest, 
-  { params }: { params: { housingId: string } }
+  { params }: { params: Promise<{ housingId: string }> }
 ) {
   try {
-    const authHeader = req.headers.get('authorization');
-    if (!authHeader) return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
-    const { housingId } = params;
+    const { housingId } = await params;
     const data = await getHousingById(housingId);
+
+    if (!data) {
+      return NextResponse.json({ error: 'Housing record not found' }, { status: 404 });
+    }
+
     return NextResponse.json({ data }, { status: 200 });
   } catch (error: any) {
-    return NextResponse.json({ error: 'Housing record not found' }, { status: 404 });
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
   }
 }
