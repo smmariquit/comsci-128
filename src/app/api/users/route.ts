567396e623d841d14678942c978c7335d7e1db0b
RDCRebugio <rcrebugio@up.edu.ph>
2026-03-20 00:06:49 +0800

    feat/create user api

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { headers } from "next/headers";
-import { getProfile } from "@/services/user-service";
+import { getProfile, createUser } from "@/services/user-service";
 
 // For retrieving profile information of current user
 // Default route for /profile/api
@@ -19,7 +19,7 @@ export async function GET(request: NextRequest) {
 			);
 		}
 		// Check request/call user service
-		const user = await getProfile(userId);
+		const user = await getProfile(Number(userId));
 
 		// Send Response
 		if (!user) {
@@ -37,6 +37,42 @@ export async function GET(request: NextRequest) {
 		return NextResponse.json(
 			{ message: "Failed to fetch user.", error: error.message },
 			{ status: 500 },
+		);
+	}
+}
+
+// For creating a new user record -- access endpoint when signing up
+// Default route for /api/users
+export async function POST(request: NextRequest) {
+	try {
+		// Check auth
+		const headersList = await headers();
+		const userId = headersList.get("x-user-id");
+
+		if (!userId) {
+			return NextResponse.json(
+				{ message: "Unauthorized: Invalid Token." },
+				{ status: 401 }
+			);
+		}
+
+		// Get request body
+		const body = await request.json();
+
+		// Call user service
+		const newUser = await createUser(body);
+
+		// OK Response upon successful creation
+		return NextResponse.json(
+			{ message: "User created successfully.", user: newUser },
+			{ status: 201 }
+		);
+
+	} catch (error: any) {
+		console.error("Error creating user:", error);
+		return NextResponse.json(
+			{ message: error.message || "Failed to create user." },
+			{ status: 500 }
 		);
 	}
 }
