560f9f415a94d07213c331fdd13ac507a0341669
GiovannApolinar <gnapolinar1@up.edu.ph>
2026-03-20 10:39:22 +0800

    feat(service): addition of get all user

--- src/app/lib/services/user-service.ts
+++ src/app/lib/services/user-service.ts
@@ -1,4 +1,4 @@
-import { findUserById, User } from "@/data/user";
+import { findUserById, findAllUsers, User } from "@/data/user";
 
 // getProfile - INPUT: userId | OUTPUT: user (if found), null/error (if not)
 export const getProfile = async (userId: string): Promise<User | null> => {
@@ -8,6 +8,19 @@ export const getProfile = async (userId: string): Promise<User | null> => {
 		if (!userProfile) return null;
 
 		return userProfile;
+	} catch (error) {
+		console.error("Error: ", error);
+		throw new Error("Error");
+	}
+};
+
+export const getAllProfile = async (): Promise<User[] | null> => {
+	try {
+		const userProfiles = await findAllUsers();
+
+		if (!userProfiles) return null;
+
+		return userProfiles;
 	} catch (error) {
 		console.error("Error: ", error);
 		throw new Error("Error");
