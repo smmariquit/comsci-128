fe47d1441606e58ed233163c7f915a5b5522f18a
aquinovgb <vbaquino@up.edu.ph>
2026-03-21 21:21:42 +0800

    fix: typo error

--- src/app/lib/services/room-service.ts
+++ src/app/lib/services/room-service.ts
@@ -4,8 +4,10 @@ import { Room } from "@/app/lib/models/room";
 // Service logic for removing a room.
 // Ensures the room exists and is empty before performing a soft delete.
 export const removeRoom = async (roomId: number): Promise<Room | null> => {
+    console.log("Service received roomId:", roomId, "Type:", typeof roomId);
     try {
         const room = await findRoomById(roomId);
+        console.log("Room found in DB:", room); // This will show you if it's null or already deleted
 
         if (!room) {
             return null;
