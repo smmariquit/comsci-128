4e2970598ffa81fa35f8d3cc8b9ce6a9c35c13ef
GiovannApolinar <gnapolinar1@up.edu.ph>
2026-03-21 12:24:51 +0800

    feat: adds service layer for updating housing

--- src/app/lib/services/housing-service.ts
+++ src/app/lib/services/housing-service.ts
@@ -1,4 +1,4 @@
-import { createHousing, findAllHousing } from "@/data/housing";
+import { createHousing, findAllHousing, updateHousing } from "@/data/housing";
 import { Housing } from "@/models/housing";
 
 // getProfile - INPUT: userId | OUTPUT: user (if found), null/error (if not)
@@ -24,6 +24,20 @@ export const addHousing = async (
 		if (!housingDetail) return null;
 
 		return housingDetail;
+	} catch (error) {
+		console.error("Error: ", error);
+		throw new Error("Error");
+	}
+};
+
+export const modifyHousing = async (
+	housingId: Number,
+	HousingDetail: Housing,
+): Promise<Housing | null> => {
+	try {
+		const housingDetail = await updateHousing(housingId, HousingDetail);
+
+		return housingDetail ?? null;
 	} catch (error) {
 		console.error("Error: ", error);
 		throw new Error("Error");
