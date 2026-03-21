5d79aae66dcb99c45a5badee5b859dab5ff4dcf4
aquinovgb <vbaquino@up.edu.ph>
2026-03-21 20:25:43 +0800

    fix: update incorrect database table reference

--- src/app/lib/models/room.ts
+++ src/app/lib/models/room.ts
@@ -1,6 +1,6 @@
 import { Tables, TablesInsert, TablesUpdate } from "@/app/types/database.types";
 
 // Define Room record based on DB schema
-export type Room = Tables<"housing">;
-export type RoomInsert = TablesInsert<"housing">;
-export type RoomUpdate = TablesUpdate<"housing">;
+export type Room = Tables<"room">;
+export type RoomInsert = TablesInsert<"room">;
+export type RoomUpdate = TablesUpdate<"room">;
