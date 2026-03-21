7c32e03c2ba0e4af480518a0fb660b149092fe41
aquinovgb <vbaquino@up.edu.ph>
2026-03-21 23:01:41 +0800

    feat: added remove housing service

--- src/app/lib/services/housing-service.ts
+++ src/app/lib/services/housing-service.ts
@@ -1,4 +1,4 @@
-import { createHousing, findAllHousing } from "@/data/housing";
+import { createHousing, findAllHousing, deleteHousing, findHousingById } from "@/data/housing";
 import { Housing } from "@/models/housing";
 
 // getProfile - INPUT: userId | OUTPUT: user (if found), null/error (if not)
@@ -28,4 +28,29 @@ export const addHousing = async (
 		console.error("Error: ", error);
 		throw new Error("Error");
 	}
+};
+
+export const removeHousing = async (housingId: number): Promise<Housing | null> => {
+    try {
+        const housing = await findHousingById(housingId.toString());
+        if (!housing) {
+            throw new Error("Housing record not found or already deactivated.");
+        }
+
+        /**
+         * TODO: Integration Task
+         * Once 'room-delete' PR is merged, 
+         * add cascading soft delete for rooms of housing
+         */
+
+        const deactivatedHousing = await deleteHousing(housingId);
+        return deactivatedHousing ?? null;
+    } catch (error: any) {
+        if (error.message.includes("not found")) {
+            throw error; 
+        }
+        
+        console.error("Service Error (removeHousing): ", error.message);
+        throw new Error("Failed to deactivate housing record due to a system error.");
+    }
 };
