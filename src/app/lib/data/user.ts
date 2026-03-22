63d44b47220fa318a1a3260bab46123ad3baba66
Zel <cysilerio@up.edu.ph>
2026-03-22 12:55:23 +0800

    refactor(user): revise user type in create user

--- src/app/lib/data/user.ts
+++ src/app/lib/data/user.ts
@@ -1,97 +1,25 @@
-import { Database, Tables, Enums } from "../../types/database.types";
+import { User } from "@/models/user";
 import { supabase } from "../supabase";
 
-type User = Tables<'user'>;
-
-//This serves as the mock 'Database' to test functions
-// const MOCK_USERS: User[] = [
-// 	{
-// 		userId: "1",
-// 		email: "nahidadapt@mappa.com",
-
-// 		firstName: "Mahoraga",
-// 		lastName: "Shikigami",
-// 		permAddress: "Shibuya, Tokyo, Japan",
-// 		contactNumber: "+81-90-0000-0001",
-
-// 		role: "Student",
-// 		status: "Active",
-// 	},
-// 	{
-// 		userId: "2",
-// 		email: "johnkaisen@mappa.com",
-
-// 		firstName: "John",
-// 		lastName: "Kaisen",
-// 		permAddress: "Shinjuku, Tokyo, Japan",
-// 		contactNumber: "+81-90-0000-0002",
-// 		contactEmail: "john.personal@example.com",
-
-// 		role: "Student",
-// 		status: "Inactive",
-// 	},
-// 	{
-// 		userId: "3",
-// 		email: "susangrotto@mgmail.com",
-
-// 		firstName: "Susan",
-// 		lastName: "Grotto",
-// 		permAddress: "Kyoto, Japan",
-// 		contactNumber: "+81-90-0000-0003",
-// 		sex: "Female",
-
-// 		role: "Landlord",
-// 		status: "Active",
-// 	},
-// 	{
-// 		userId: "4",
-// 		email: "alfredbuttler@wayne.com",
-
-// 		firstName: "Bruce",
-// 		middleName: "Thomas",
-// 		lastName: "Wayne",
-// 		permAddress: "Gotham City",
-// 		contactNumber: "+1-202-555-0104",
-// 		birthday: new Date("1985-02-19"),
-// 		age: 40,
-
-// 		role: "Housing Administrator",
-// 		status: "Active",
-// 	},
-// 	{
-// 		userId: "5",
-// 		email: "allancruz@gov.ph",
-
-// 		firstName: "Allan",
-// 		lastName: "Cruz",
-// 		permAddress: "Quezon City, Philippines",
-// 		contactNumber: "+63-917-000-0005",
-// 		sex: "Male",
-
-// 		role: "Dormitory Manager",
-// 		status: "Active",
-// 	},
-// ];
-
-export async function createUser(userDetails: any[], userType: string, password: string): Promise<number | null> {
+export async function createUser(userDetails: User, userType: string, userPassword: string): Promise<number | null> {
 	// RETURNS the PK of the newly inserted user
 
 	const { data, error } = await supabase
 		.from('user')
-		.insert([{ 
-			account_email: userDetails[0],
-			password: password,
-			first_name: userDetails[1],
-			middle_name: userDetails[2] ?? null, // unless null can be passed
-			last_name: userDetails[3],
-			birthday: userDetails[4] ?? null,
-			home_address: userDetails[5] ?? null,
-			phone_number: userDetails[6] ?? null,
-			contact_email: userDetails[7] ?? null,
-			sex: userDetails[8],
+		.insert({ 
+			account_email: userDetails.accountEmail,
+			password: userPassword,
+			first_name: userDetails.firstName,
+			middle_name: userDetails.middleName ?? null,
+			last_name: userDetails.lastName,
+			birthday: userDetails.birthday ?? null,
+			home_address: userDetails.permAddress ?? null,
+			phone_number: userDetails.contactNumber ?? null,
+			contact_email: userDetails.contactEmail ?? null,
+			sex: userDetails.sex ?? null,
 			userType: userType,
 			is_deleted: false
-		}])
+		})
 		.select('account_number');
 
 	if (error) {
@@ -125,7 +53,7 @@ export async function deactivateUser(userId: string): Promise<User | null> {
 	const { data, error } = await supabase
 		.from('user')
 		.update({ is_deleted: true })
-		.eq('account_number', userId)
+		.eq('account_number', Number(userId))
 		.select();
 
 	if (error) {
@@ -154,11 +82,10 @@ export async function findUserById(userId: string): Promise<User | null> {
 	//This function takes a USERID of type STRING.
 	// RETURNS a USER object when found in the DB, otherwise return null.
 
-	userId = '1'; // test
 	const { data, error } = await supabase
 		.from('user')
 		.select()
-		.eq('account_number', userId);
+		.eq('account_number', Number(userId));
 
 	if (error) {
 		throw new Error(error.message);
