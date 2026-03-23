a25cfd6b0be30fec802e018508ff69d07309a433
yhannis4 <yoprudencio@up.edu.ph>
2026-03-24 05:29:32 +0800

    feat: added room service layer for room creation

--- src/app/lib/data/room.ts
+++ src/app/lib/data/room.ts
@@ -4,7 +4,7 @@ import {Room, RoomInsert} from "@/models/room";
 
 export type NewRoom = Omit<Room, "room_id" | "is_deleted">
 
-export async function createRoom(data: NewRoom): Promise<Room> {
+export async function createRoom(data: RoomInsert): Promise<Room> {
   const { data: newRecord, error } = await supabase
     .from("room")
     .insert(data)
