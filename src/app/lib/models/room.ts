0326e84d8b564eff8d2c509576ae6091314d6805
yhannis4 <yoprudencio@up.edu.ph>
2026-03-24 05:21:10 +0800

    feat: added Room CREATE supabase query

---
+++ src/app/lib/models/room.ts
@@ -0,0 +1,6 @@
+import { Tables, TablesInsert, TablesUpdate } from "@/app/types/database.types";
+
+// Define Room record based on DB schema
+export type Room = Tables<"room">;
+export type RoomInsert = TablesInsert<"room">;
+export type RoomUpdate = TablesUpdate<"room">;
