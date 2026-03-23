548bf82aaf0ac662b99b8fb17a11182bd69b5e24
PaulFababeir <pffababeir@up.edu.ph>
2026-03-23 14:26:56 +0800

    feat: shows get room info for frontend

--- src/app/(main)/manage/accommodations/[id]/[unit]/page.tsx
+++ src/app/(main)/manage/accommodations/[id]/[unit]/page.tsx
@@ -1,9 +1,34 @@
 import Link from 'next/link';
+import { getRoomById } from "@/app/lib/data/room";
 
-export default function Page() {
+export default async function Page() {
+  const testId = 5;
+
+  let room = null;
+  try {
+    room = await getRoomById(testId);
+  } catch (error) {
+    console.error("Error: ", error);
+  }
+
   return (
     <main className="min-h-screen  text-white flex flex-col items-center justify-center p-6">
       <h1 className="text-4xl font-bold text-center mb-8">Manage Accommodation Unit Page</h1>
+      
+      {/* Basic Detail Display */}
+      <div className="bg-slate-800 p-6 rounded-lg border border-slate-700 w-80 mb-8">
+        {room ? (
+          <ul className="space-y-2">
+            <li><strong>Room ID:</strong> {room.room_id}</li>
+            <li><strong>Type:</strong> {room.room_type}</li>
+            <li><strong>Max Occupants:</strong> {room.maximum_occupants}</li>
+            <li><strong>Status:</strong> {room.occupancy_status}</li>
+          </ul>
+        ) : (
+          <p className="text-gray-400">Room {testId} not found in DB.</p>
+        )}
+      </div>
+      
       <div className="flex gap-4 flex-wrap justify-center">
         <Link href="/manage/accommodations/1/unit-a/occupants" className="bg-white text-black px-6 py-2 rounded font-bold hover:bg-gray-200">
           Occupants
