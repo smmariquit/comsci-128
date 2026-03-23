36143bb124d729f2b022e39da311e1deb281d95a
yhannis4 <yoprudencio@up.edu.ph>
2026-03-24 05:21:10 +0800

    feat: added Room CREATE supabase query

---
+++ src/app/lib/data/room.ts
@@ -0,0 +1,16 @@
+
+import { supabase } from "@/lib/supabase";
+import {Room, RoomInsert} from "@/models/room";
+
+export type NewRoom = Omit<Room, "room_id" | "is_deleted">
+
+export async function createRoom(data: NewRoom): Promise<Room> {
+  const { data: newRecord, error } = await supabase
+    .from("room")
+    .insert(data)
+    .select()
+    .single()
+
+  if (error) throw new Error(error.message)
+  return newRecord
+}
