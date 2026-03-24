bb2e33d7e373d8a9efe730bc477e8158e6a55b86
Robin Drey Rebugio <rcrebugio@up.edu.ph>
2026-03-24 23:03:24 +0800

    fix: moved getroom api to the other folder

--- src/app/api/rooms/[roomId]/route.ts
+++ src/app/api/rooms/[roomId]/route.ts
@@ -1,5 +1,40 @@
 import { NextRequest, NextResponse } from "next/server";
-import { removeRoom } from "@/app/lib/services/room-service";
+import { removeRoom, getRoom } from "@/app/lib/services/room-service";
+
+export async function GET(
+    _request: NextRequest,
+    { params }: { params: Promise<{ id: string }> },
+) {
+    try {
+        /*
+            TODO:
+            - authentication middleware
+            - role access (role guard) middleware
+        */
+        const { id } = await params;
+
+        // Check request/call user service
+        const room = await getRoom(Number(id));
+
+        // Send Response
+        if (!room) {
+            // room not found
+            return NextResponse.json(
+                { message: "Room not found." },
+                { status: 404 },
+            );
+        }
+
+        // User found
+        return NextResponse.json(room, { status: 200 });
+    } catch (error: any) {
+        console.error("Error fetching room", error);
+        return NextResponse.json(
+            { message: "Failed to fetch room.", error: error.message },
+            { status: 500 },
+        );
+    }
+}
 
 export async function DELETE(
     req: NextRequest,
