f6c44f342d31093b98090c99a33ad4dfc53928f7
Robin Drey Rebugio <rcrebugio@up.edu.ph>
2026-03-24 22:58:54 +0800

    feat: getroom api

--- src/app/api/rooms/route.ts
+++ src/app/api/rooms/route.ts
@@ -1,7 +1,7 @@
 
 
 import { NextRequest, NextResponse } from "next/server";
-import { addRoom } from "@/services/room-service";
+import { addRoom, getRoom } from "@/services/room-service";
 
 export async function POST(request: NextRequest) {
 
@@ -30,3 +30,41 @@ export async function POST(request: NextRequest) {
     )
   }
 }
+
+// fetch only one room details
+export async function GET(
+	_request: NextRequest,
+	{ params }: { params: Promise<{ id: string }> },
+) {
+	try {
+		/*
+            TODO:
+            - authentication middleware
+            - role access (role guard) middleware
+        */
+		const { id } = await params;
+
+		// Check request/call user service
+		const room = await getRoom(Number(id));
+
+		// Send Response
+		if (!room) {
+			// room not found
+			return NextResponse.json(
+				{ message: "Room not found." },
+				{ status: 404 },
+			);
+		}
+
+		// User found
+		return NextResponse.json(room, { status: 200 });
+	} catch (error: any) {
+		console.error("Error fetching room", error);
+		return NextResponse.json(
+			{ message: "Failed to fetch room.", error: error.message },
+			{ status: 500 },
+		);
+	}
+}
+
+// fetch all rooms
