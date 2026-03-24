bc27c52a42bf5036459a55dedda8764e7d49532f
Robin Drey Rebugio <rcrebugio@up.edu.ph>
2026-03-24 21:52:57 +0800

    style: removed mock data

--- src/app/lib/data/user.ts
+++ src/app/lib/data/user.ts
@@ -1,65 +1,6 @@
 import { Tables, TablesInsert, TablesUpdate } from "@/app/types/database.types";
 import { supabase } from "../supabase";
 
-//This serves as the mock 'Database' to test functions
-// const MOCK_USERS: User[] = [
-// 	{
-// 		account_number: 1,
-// 		account_email: "mahoraga@mappa.com",
-// 		first_name: "Mahoraga",
-// 		last_name: "Shikigami",
-// 		home_address: "Shibuya, Tokyo, Japan",
-// 		phone_number: "+81-90-0000-0001",
-// 		user_type: "Student",
-// 		is_deleted: false,
-// 	},
-// 	{
-// 		account_number: 2,
-// 		account_email: "johnkaisen@mappa.com",
-// 		first_name: "John",
-// 		last_name: "Kaisen",
-// 		home_address: "Shinjuku, Tokyo, Japan",
-// 		phone_number: "+81-90-0000-0002",
-// 		contact_email: "john.personal@example.com",
-// 		user_type: "Student",
-// 		is_deleted: false,
-// 	},
-// 	{
-// 		account_number: 3,
-// 		account_email: "susangrotto@mgmail.com",
-// 		first_name: "Susan",
-// 		last_name: "Grotto",
-// 		home_address: "Kyoto, Japan",
-// 		phone_number: "+81-90-0000-0003",
-// 		sex: "Female",
-// 		user_type: "Manager",
-// 		is_deleted: false,
-// 	},
-// 	{
-// 		account_number: 4,
-// 		account_email: "alfredbuttler@wayne.com",
-// 		first_name: "Bruce",
-// 		middle_name: "Thomas",
-// 		last_name: "Wayne",
-// 		home_address: "Gotham City",
-// 		phone_number: "+1-202-555-0104",
-// 		birthday: new Date("1985-02-19"),
-// 		user_type: "Manager",
-// 		is_deleted: false,
-// 	},
-// 	{
-// 		account_number: 5,
-// 		account_email: "allancruz@gov.ph",
-// 		first_name: "Allan",
-// 		last_name: "Cruz",
-// 		home_address: "Quezon City, Philippines",
-// 		phone_number: "+63-917-000-0005",
-// 		sex: "Male",
-// 		user_type: "Manager",
-// 		is_deleted: false,
-// 	},
-// ];
-
 export async function findUserById(userId: Number): Promise<User | null> {
 
     const { data, error } = await supabase
