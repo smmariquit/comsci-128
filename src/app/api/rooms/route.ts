0fc35674e449ab3d858d1682a78d75e0e3e3fe2f
GiovannApolinar <gnapolinar1@up.edu.ph>
2026-03-25 15:03:37 +0800

    refactor: aligns import to path alias

--- src/app/api/rooms/route.ts
+++ src/app/api/rooms/route.ts
@@ -1,34 +1,40 @@
-
-
 import { NextRequest, NextResponse } from "next/server";
-import { addRoom, fetchAllRooms, getRoom } from "@/services/room-service";
+import { addRoom, fetchAllRooms } from "@/services/room-service";
 
 export async function POST(request: NextRequest) {
+	try {
+		const authHeader = request.headers.get("authorization");
+		if (!authHeader) {
+			return NextResponse.json(
+				{ error: "Unauthorized" },
+				{ status: 401 },
+			);
+		}
 
-  try {
-    const authHeader = request.headers.get("authorization")
-    if (!authHeader) {
-      return NextResponse.json({ error: "Unauthorized" }, { status: 401 })
-    }
+		const body = await request.json();
 
-    const body = await request.json()
-    
-    if (!body) {
-      return NextResponse.json({ message: "Room data is required." }, { status: 400 })
-    }
+		if (!body) {
+			return NextResponse.json(
+				{ message: "Room data is required." },
+				{ status: 400 },
+			);
+		}
 
-    const newRoom = await addRoom(body)
-    if (!newRoom) return NextResponse.json({ message: "Failed to create room." }, { status: 400 })
+		const newRoom = await addRoom(body);
+		if (!newRoom)
+			return NextResponse.json(
+				{ message: "Failed to create room." },
+				{ status: 400 },
+			);
 
-    return NextResponse.json(newRoom, { status: 201 })
-
-  } catch (error: any) {
-    console.error("Error creating room:", error)
-    return NextResponse.json(
-      { message: "Failed to create room.", error: error.message },
-      { status: 500 }
-    )
-  }
+		return NextResponse.json(newRoom, { status: 201 });
+	} catch (error: any) {
+		console.error("Error creating room:", error);
+		return NextResponse.json(
+			{ message: "Failed to create room.", error: error.message },
+			{ status: 500 },
+		);
+	}
 }
 
 // fetch all rooms
@@ -39,7 +45,6 @@ export async function GET(_request: NextRequest) {
             - authentication middleware
             - role access (role guard) middleware
         */
-
 
 		// Check request/call user service
 		const room = await fetchAllRooms();
@@ -63,4 +68,3 @@ export async function GET(_request: NextRequest) {
 		);
 	}
 }
-
