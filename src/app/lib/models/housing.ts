06a9cfee6cfe186e1d6a891d4452cd9711a51b54
GiovannApolinar <gnapolinar1@up.edu.ph>
2026-03-26 12:14:58 +0800

    refactor: adds join type for housing with rooms

--- src/app/lib/models/housing.ts
+++ src/app/lib/models/housing.ts
@@ -1,6 +1,8 @@
 import { Tables, TablesInsert, TablesUpdate } from "@/app/types/database.types";
+import { Room } from "@/models/room";
 
 // Define Housing record based on DB schema
 export type Housing = Tables<"housing">;
+export type HousingWithRooms = Housing & { room: Room[] };
 export type HousingInsert = TablesInsert<"housing">;
 export type HousingUpdate = TablesUpdate<"housing">;
