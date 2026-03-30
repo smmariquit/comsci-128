c3dbb3802acb90ed20777065ceb90bd7d33d31b6
Jelen Corpuz <jtcorpuz5@up.edu.ph>
2026-03-30 22:23:27 +0800

    fix(ui): header on student housing

--- src/app/(main)/student/(dashboard)/page.tsx
+++ src/app/(main)/student/(dashboard)/page.tsx
@@ -10,85 +10,51 @@ export default async function DashboardPage() {
     }));
 
 	return (
+        
         <div
             style={{
                 width: "100%",
                 minHeight: "100%",
-                background: "white",
+                background: "#EDE9DE",
                 display: "inline-flex",
                 flexDirection: "column",
                 justifyContent: "flex-start",
                 alignItems: "stretch",
             }}
         >
-            <div
-                style={{
-                    height: 108,
-                    paddingLeft: 18,
-                    paddingRight: 18,
-                    background: "#567375",
-                    overflow: "hidden",
-                    justifyContent: "space-between",
-                    alignItems: "center",
-                    display: "inline-flex",
-                }}
-            >
-                <div style={{ display: "flex", alignItems: "center", gap: 18 }}>
-                    <div
-                        style={{
-                            width: 215,
-                            height: 72,
-                            paddingLeft: 36,
-                            paddingRight: 36,
-                            paddingTop: 10,
-                            paddingBottom: 10,
-                            background: "white",
-                            display: "flex",
-                            alignItems: "center",
-                        }}
-                    >
-                        <div style={{ color: "#1C2632", fontSize: 40, fontFamily: "DM Sans", fontWeight: "600" }}>Title</div>
-                    </div>
-                    <div style={{ color: "black", fontSize: 24, fontFamily: "DM Mono" }}>Dashboard</div>
-                    <div style={{ width: 2, height: 36, background: "black" }} />
-                    <Link
-                        href="/student/housing"
-                        style={{ color: "black", fontSize: 24, fontFamily: "DM Mono", textDecoration: "none" }}
-                    >
-                        Browse
-                    </Link>
+            {/* NAV BAR */}
+            <header className="flex h-[10vh] w-full max-w-[1440px] bg-[#1C2632] items-center justify-between px-10 text-m">
+  
+                <div className="flex items-center gap-8">
+                    <h1 className="text-[#EDE9DE] text-xl font-semibold tracking-tight">Title</h1>
+                    
+                    <nav className="flex items-center gap-6 border-l border-gray-700 pl-8 ">
+                    <a href="/student/dashboard" className="text-[#EDE9DE]">Dashboard</a>
+                    <a href="/student" className="text-[#EDE9DE] transition-colors">Accommodations</a>
+                    </nav>
                 </div>
-                <div
-                    style={{
-                        width: 260,
-                        height: 72,
-                        padding: 12,
-                        background: "white",
-                        display: "flex",
-                        justifyContent: "space-between",
-                        alignItems: "center",
-                    }}
-                >
-                    <div style={{ color: "black", fontSize: 24, fontFamily: "DM Mono" }}>{currUser?.first_name ?? "Username"}</div>
-                    <div style={{ width: 48, height: 48, background: "#6B6B6B" }} />
+
+                <div className="flex items-center gap-6">
+                    <button className="text-[#EDE9DE]">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+                    </svg>
+                    </button>
+
+                    <div className="h-8 w-8 aspect-square rounded-full bg-[#567375] cursor-pointer transition-all"></div>
+                </div>
+
+            </header>
+
+            {/* BREAD CRUMBS */}
+            <div className="flex flex-col bg-[#567375] px-[36px] py-[6px] justify-center">
+
+                <div className="text-[#EDE9DE] text-[13px] font-sans font-regular">
+                    Housing Browser
                 </div>
             </div>
 
-            <div
-                style={{
-                    height: 54,
-                    paddingLeft: 36,
-                    paddingRight: 36,
-                    paddingTop: 10,
-                    paddingBottom: 10,
-                    background: "#1C2632",
-                    display: "inline-flex",
-                    alignItems: "center",
-                }}
-            >
-                <div style={{ color: "#C9642A", fontSize: 40, fontFamily: "DM Sans", fontWeight: "600" }}>Housing Browser</div>
-            </div>
-
+            {/*  */}
             <div style={{ padding: 24, background: "#EDE9DE" }}>
                 <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fill, minmax(260px, 1fr))", gap: 16 }}>
                     {cards.map((card) => (
