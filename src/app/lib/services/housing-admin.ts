adbbac77c0b7430586295dec150a4bf3ff518e9e
RDCRebugio <butil@butil.localdomain>
2026-03-31 10:46:35 +0800

    feat: service landlord student housing admin

--- src/app/lib/services/housing-admin.ts
+++ src/app/lib/services/housing-admin.ts
@@ -1,6 +1,6 @@
 import { NewUser } from "@/models/user";
 import { createManager } from "../data/manager-data";
-import { createLandlord} from "../data/landlord-data";
+import { createHousingAdmin } from "../data/housing_admin"; 
 
 const addLandlord = async (
   userDetails: NewUser,
@@ -8,7 +8,7 @@ const addLandlord = async (
 ): Promise<any> => {
   try {
     // Use your existing createLandlord function
-    const landlord = await createLandlord(userDetails, password);
+    const landlord = await createHousingAdmin(userDetails, password);
     return landlord;
   } catch (error) {
     console.error("Error creating landlord: ", error);
