f726caa7b413c5fcc4054a5099a75f4a4c3e1406
RDCRebugio <butil@butil.localdomain>
2026-03-31 11:49:30 +0800

    fix: working student register

--- src/app/lib/services/student-service.ts
+++ src/app/lib/services/student-service.ts
@@ -1,14 +1,14 @@
-import { userService } from "./user-service";
 import { NewUser, User } from "../models/user";
 import { createStudent } from "../data/student-data";
 
 export const addStudent = async (
     userDetails: NewUser,
-    password: string
+    password: string,
+    student_number: Number,
 ): Promise<any> => {
     try {
         // Use your existing createStudent function
-        const student = await createStudent(userDetails, password);
+        const student = await createStudent(userDetails, password, student_number);
         return student;
     } catch (error) {
         console.error("Error creating student: ", error);
