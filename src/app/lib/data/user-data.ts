981c3cbba22553c3ede29caf679097680eccfcb6
Zel <cysilerio@up.edu.ph>
2026-04-06 01:11:34 +0800

    refactor(user): Change function names

--- src/app/lib/data/user-data.ts
+++ src/app/lib/data/user-data.ts
@@ -1,7 +1,7 @@
 import { User, NewUser, UpdateUser } from "@/models/user";
 import { supabase } from "../supabase";
 
-async function create(userDetails: NewUser): Promise<User> {
+async function createUser(userDetails: NewUser): Promise<User> {
 	// this is for returning the newly inserted user
 	const { data, error } = await supabase
 		.from("user")
@@ -13,11 +13,10 @@ async function create(userDetails: NewUser): Promise<User> {
 		throw new Error(error.message);
 	}
 
-	return data;
-	// return data.account_number if PK
+	return data.account_number;
 }
 
-async function findAll(): Promise<User[]> {
+async function findAllUsers(): Promise<User[]> {
 	// RETURNS an array of USER rows when found in the DB; otherwise, returns null.
 
 	const { data, error } = await supabase.from("user").select();
@@ -29,7 +28,7 @@ async function findAll(): Promise<User[]> {
 	return data ?? null;
 }
 
-async function findById(userId: number): Promise<User | null> {
+async function findUserById(userId: number): Promise<User | null> {
 	const { data, error } = await supabase
 		.from("user")
 		.select()
@@ -43,7 +42,7 @@ async function findById(userId: number): Promise<User | null> {
 	return data;
 }
 
-async function findByEmail(userEmail: string): Promise<User | null> {
+async function findUserByEmail(userEmail: string): Promise<User | null> {
 	const { data, error } = await supabase
 		.from("user")
 		.select()
@@ -57,7 +56,7 @@ async function findByEmail(userEmail: string): Promise<User | null> {
 	return data;
 }
 
-async function update(
+async function updateUser(
 	userId: number,
 	userDetails: UpdateUser,
 ): Promise<UpdateUser | null> {
@@ -77,7 +76,7 @@ async function update(
 	return data && data.length > 0 ? data[0] : null;
 }
 
-async function deactivateById(userId: number): Promise<UpdateUser | null> {
+async function deactivateUserById(userId: number): Promise<UpdateUser | null> {
 	//This function takes a USERID of type STRING.
 	// CHANGES is_deleted field to true if user is found, otherwise return null.
 
@@ -96,10 +95,10 @@ async function deactivateById(userId: number): Promise<UpdateUser | null> {
 }
 
 export const userData = {
-	create,
-	findAll,
-	findById,
-	findByEmail,
-	update,
-	deactivateById,
+	createUser,
+	findAllUsers,
+	findUserById,
+	findUserByEmail,
+	updateUser,
+	deactivateUserById,
 };
