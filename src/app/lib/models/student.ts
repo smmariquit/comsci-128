a9502eef72b2dd5b56c4bbd52641dea3e56723a8
Zel <cysilerio@up.edu.ph>
2026-04-06 01:09:14 +0800

    feat(student_academic): Create student academic models

--- src/app/lib/models/student.ts
+++ src/app/lib/models/student.ts
@@ -3,11 +3,3 @@ import { Tables, TablesInsert, TablesUpdate } from "@/app/types/database.types";
 export type Student = Tables<"student">;
 export type NewStudent = TablesInsert<"student">;
 export type UpdateStudent = TablesUpdate<"student">;
-
-export type StudentAcademic = Tables<"student_academic">;
-export type NewStudentAcademic = TablesInsert<"student_academic">;
-export type UpdateStudentAcademic = TablesUpdate<"student_academic">;
-
-export type StudentAccommodationHistory = Tables<"student_accommodation_history">;
-export type NewStudentAccommodationHistory = TablesInsert<"student_accommodation_history">;
-export type UpdateStudentAccommodationHistory = TablesUpdate<"student_accommodation_history">;
