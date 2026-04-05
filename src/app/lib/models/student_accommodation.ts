15fb86eb9c35ca98a2dac2e000e9a05b95501c97
Zel <cysilerio@up.edu.ph>
2026-04-06 01:09:58 +0800

    feat(student_accommodation_history): Create student accommodation history models

---
+++ src/app/lib/models/student_accommodation.ts
@@ -0,0 +1,5 @@
+import { Tables, TablesInsert, TablesUpdate } from "@/app/types/database.types";
+
+export type StudentAccommodationHistory = Tables<"student_accommodation_history">;
+export type NewStudentAccommodationHistory = TablesInsert<"student_accommodation_history">;
+export type UpdateStudentAccommodationHistory = TablesUpdate<"student_accommodation_history">;
