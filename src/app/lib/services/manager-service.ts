e82b083015c342aee5aa6fa4097755b6f3c55459
Robin Drey Rebugio <rcrebugio@up.edu.ph>
2026-04-06 23:51:00 +0800

    feat: get all manager service

--- src/app/lib/services/manager-service.ts
+++ src/app/lib/services/manager-service.ts
@@ -1,2 +1,28 @@
+import { User } from "@/models/user";
+import { getAllLandlords } from "../data/landlord-data";
+import { getAllHousingAdmins } from "../data/housing_admin";
 
-const 
+type ServiceResponse<T> = { data?: T; error?: string };
+
+const getAllManagers = async (): Promise<User[] | null> => {  // 👈 fixed
+    try {
+        const { data: landlordData } = await getAllLandlords();
+        const { data: housingAdminData } = await getAllHousingAdmins();
+
+        if (!landlordData || !housingAdminData) return [];
+
+        // extract each user object in landlord/housingadmin
+        const allProfiles = [...landlordData, ...housingAdminData]
+            .map((profile: any) => profile.manager?.user)
+            .filter(Boolean);
+
+        return allProfiles;
+    } catch (error) {
+        console.error("Error: ", error);
+        throw new Error("Error");
+    }
+};
+
+export const managerService = {
+    getAllManagers
+};
