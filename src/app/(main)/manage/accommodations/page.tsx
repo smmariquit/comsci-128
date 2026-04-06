17d3dcdde26ca4db9203a692b6e1f89276fb2db4
Jehmuel <pecenteno@up.edu.ph>
2026-04-06 16:05:29 +0800

    style: refine UI styling

--- src/app/(main)/manage/accommodations/page.tsx
+++ src/app/(main)/manage/accommodations/page.tsx
@@ -11,8 +11,8 @@ function DetailItem({
 }) {
   return (
     <div className="flex flex-col flex-1 items-center">
-      <span className="text-sm text-gray-500">{label}</span>
-      <span className="font-medium">{value}</span>
+      <span className="text-medium text-[var(--teal)]">{label}</span>
+      <span className="text-lg text-[var(--dark-orange) font-semibold">{value}</span>
     </div>
   );
 }
@@ -26,20 +26,20 @@ function AccommodationCard({
   id: number;
   name: string;
   image: string;
-  details: {label: string; value: string | number} [];
+  details: { label: string; value: string | number }[];
 }) {
   return (
     <Link href={`/manage/accommodations/${id}`}>
-      <div className="flex items-center gap-4 border rounded-lg p-4 text-white bg-black">
+      <div className="flex items-center gap-6 rounded-xl p-6 bg-[var(--cream)] text-[var(--dark-orange)] shadow-sm border-l-8 border-[var(--teal)] hover:shadow-md transition">
 
-        <div className="w-25 h-25 bg-gray-300 rounded">
-          <img src={image} className="w-full h-full object-cover rounded" />
+        <div className="w-35 h-35 bg-gray-300 rounded-lg overflow-hidden">
+          <img src={image} className="w-full h-full object-cover" />
         </div>
 
-
-        <div className="flex flex-col w-full ">
-
-          <div className="font-semibold mb-2">{name}</div>
+        <div className="flex flex-col w-full gap-2">
+          <div className="font-semibold text-2xl text-[var(--dark-blue)]">
+            {name}
+          </div>
 
           <div className="flex w-full">
             {details.map((detail, index) => (
@@ -63,16 +63,17 @@ export default async function AccommodationsPage() {
   const housings = await housingService.getAllHousingWithRooms()
 
   return (
-    <main className="min-h-screen flex flex-col p-6 gap-6">
+    <main className="min-h-screen flex flex-col p-6 gap-6 bg-[var(--cream)]">
+      {/*}
       <section className="flex items-center justify-between">
-        <h1 className="text-4xl font-bold text-gray-500">Accommodations</h1>
         <Link href="/manage" className="text-sm text-gray-500 hover:underline">
           ← Back to Dashboard
         </Link>
       </section>
+      */}
 
-      <section className="flex flex-col gap-4 px-10">
-        <h1 className="text-2xl font-semibold">Accommodations List</h1>
+      <section className="flex flex-col gap-4 px-5">
+        <h1 className="text-3xl text-[var(--dark-orange)] font-semibold">Accommodations List</h1>
 
         {/* filter */}
         <div className="bg-gray-200 h-10 rounded flex items-center px-3 text-sm text-gray-600">
@@ -80,35 +81,38 @@ export default async function AccommodationsPage() {
         </div>
       </section>
 
-      <section>
+      <section className="px-5">
         {!housings || housings.length === 0 ? (
           <p className="text-gray-500">No accommodations found.</p>
         ) : (
-          housings.map((housing) => {
-            const totalOccupants = housing.room.reduce(
-              (sum, r) => sum + (r.maximum_occupants ?? 0),
-              0
-            );
+          <div className="flex flex-col gap-6 bg-[var(--cream)] p-6 rounded-xl">
 
-            const freeSlots = housing.room.filter(
-              (r) => r.occupancy_status === "Empty"
-            ).length;
+            {housings.map((housing) => {
+              const totalOccupants = housing.room.reduce(
+                (sum, r) => sum + (r.maximum_occupants ?? 0),
+                0
+              );
 
-            return (
-              <AccommodationCard
-                key={housing.housing_id}
-                id={housing.housing_id}
-                name={housing.housing_name}
-                image="/assets/placeholders/housing-card.svg"
-                details={[
-                  { label: "Total Occupants", value: totalOccupants },
-                  { label: "Free Slots", value: freeSlots },
-                ]}
-              />
-            );
-          })
+              const freeSlots = housing.room.filter(
+                (r) => r.occupancy_status === "Empty"
+              ).length;
+
+              return (
+                <AccommodationCard
+                  key={housing.housing_id}
+                  id={housing.housing_id}
+                  name={housing.housing_name}
+                  image="/assets/placeholders/housing-card.svg"
+                  details={[
+                    { label: "Total Occupants", value: totalOccupants },
+                    { label: "Free Slots", value: freeSlots },
+                  ]}
+                />
+              );
+            })}
+
+          </div>
         )}
       </section>
     </main>
-  )
-}
+  )};
