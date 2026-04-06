fb98c7c3403d81c68dd92a10efd58a12a45ba624
Robin Drey Rebugio <rcrebugio@up.edu.ph>
2026-04-06 23:28:11 +0800

    chore: remove unnecessary add manager in service

--- src/app/lib/services/manager-service.ts
+++ src/app/lib/services/manager-service.ts
@@ -1,17 +1,2 @@
-import { createManager } from "../data/manager-data";
-import { NewUser } from "../models/user";
 
-const addManager = async (
-  userDetails: NewUser, 
-  password: string,
-  managerType: "Landlord" | "Housing Administrator"
-): Promise<any> => {
-  try {
-    // Use your existing createManager function
-    const manager = await createManager(userDetails, password, managerType);
-    return manager;
-  } catch (error) {
-    console.error("Error creating manager: ", error);
-    throw error;
-  }
-};
+const 
