5d3be427738304b461bae524603f95413b28549a
Robin Drey Rebugio <rcrebugio@up.edu.ph>
2026-04-09 21:08:31 +0800

    feat: add active user count api

--- src/app/api/users/count/route.ts
+++ src/app/api/users/count/route.ts
@@ -2,28 +2,26 @@ import { NextResponse } from "next/server";
 import { userService } from "@/services/user-service";
 
 export async function GET() {
-	try {
-	
-		const count = await userService.getUserCount();
+    try {
+        const totalCount = await userService.getUserCount();
+        const activeCount = await userService.getActiveUserCount();
 
-        // Fail
-		if (!count) {
-			return NextResponse.json(
-				{ message: "User count not found." },
-				{ status: 404 },
-			);
-		}
+        if (totalCount === null || activeCount === null) {
+            return NextResponse.json(
+                { message: "User count not found." },
+                { status: 404 },
+            );
+        }
 
-		// Success
-		return NextResponse.json(
-			{ count },
-			{ status: 200 },
-		);
-	} catch (error: any) {
-		console.error("Error fetching user count:", error);
-		return NextResponse.json(
-			{ message: "Failed to fetch user count.", error: error.message },
-			{ status: 500 },
-		);
-	}
+        return NextResponse.json(
+            { totalCount, activeCount },
+            { status: 200 },
+        );
+    } catch (error: any) {
+        console.error("Error fetching user count:", error);
+        return NextResponse.json(
+            { message: "Failed to fetch user count.", error: error.message },
+            { status: 500 },
+        );
+    }
 }
