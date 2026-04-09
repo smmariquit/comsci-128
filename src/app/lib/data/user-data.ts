0d0cb492e86e282c3967bac2bfb2c3cab761ddd5
aquinovgb <vbaquino@up.edu.ph>
2026-04-09 18:01:37 +0800

    feat: Added data layer for fetching users for housing admin

--- src/app/lib/data/user-data.ts
+++ src/app/lib/data/user-data.ts
@@ -117,6 +117,69 @@ async function findStudents(): Promise<any[]> {
 	return data ?? null;
 }
 
+
+// get users for housing admin by tracing student accommodation history for students
+// TODO: Also query non-student users under housing admin
+async function getUsersForHousingAdmin(managedHousingIds: number[]): Promise<any[]> {
+    const { data, error } = await supabase
+        .from("user")
+        .select(`
+            account_number,
+            first_name,
+            last_name,
+            account_email,
+            phone_number,
+            user_type,
+            sex,
+            student (
+                housing_status,
+                student_accommodation_history (
+                    movein_date,
+                    room (
+                        housing_id,
+                        housing ( housing_name )
+                    )
+                )
+            )
+        `);
+
+	if (error) throw new Error(error.message);
+
+	const mappedUsers = data.map((u: any) => {
+		let currentHousingId = null;
+		let currentHousingName = undefined;
+
+		const history = u.student?.student_accommodation_history;
+
+		if (history && history.length > 0) {
+			const latestHistory = history.sort((a: any, b: any) => 
+				new Date(b.movein_date).getTime() - new Date(a.movein_date).getTime()
+			)[0];
+
+			if (latestHistory?.room) {
+				currentHousingId = latestHistory.room.housing_id;
+				currentHousingName = latestHistory.room.housing?.housing_name;
+			}
+		}
+
+		return {
+			account_number: u.account_number,
+			full_name: `${u.first_name} ${u.last_name}`,
+			account_email: u.account_email,
+			phone_number: u.phone_number,
+			user_type: u.user_type,
+			is_deleted: false, 	// TO DO: Remove this in the future
+								// Mapped is_deleted to false by default for now since it is required to render table and for filter. 
+			sex: u.sex,
+			housing_status: u.student?.housing_status || undefined,
+			housing_name: currentHousingName,
+			housing_id: currentHousingId 
+		};
+	});
+
+	return mappedUsers.filter(u => u.housing_id && managedHousingIds.includes(u.housing_id));
+}
+
 export const userData = {
 	create,
 	findAll,
@@ -125,4 +188,5 @@ export const userData = {
 	update,
 	deactivateById,
 	findStudents,
+	getUsersForHousingAdmin
 };
