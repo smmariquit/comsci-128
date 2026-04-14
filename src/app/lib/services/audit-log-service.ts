20a6cfa6266e72232b7c95ccded2475d043ef5f1
Robin Drey Rebugio <rcrebugio@up.edu.ph>
2026-04-14 13:39:12 +0800

    feat: getall audit in service

--- src/app/lib/services/audit-log-service.ts
+++ src/app/lib/services/audit-log-service.ts
@@ -1,2 +1,11 @@
 import { AuditLog, auditLogData } from "../data/audit-log-data";
 
+const getAllAuditLogs = async (): Promise<AuditLog[]> => {
+    try {
+        const auditLogs = await auditLogData.getAll();
+        return auditLogs ?? [];
+    } catch (error) {
+        console.error("Error:", error);
+        throw new Error("Failed to fetch audit logs.");
+    }
+};
