2efb17f6328199909a091a529f23a099292c664e
Robin Drey Rebugio <rcrebugio@up.edu.ph>
2026-04-14 13:38:47 +0800

    refactor: change query names

--- src/app/lib/data/audit-log-data.ts
+++ src/app/lib/data/audit-log-data.ts
@@ -4,6 +4,14 @@ export type ActionType = 'Application Status' | 'Bill Status' | 'Auth Register' 
 | 'Update User Role' | 'Submit Application' | 'Update Application Status' | 'Withdraw Application' | 'Create Housing' | 'Update Housing' |
 'Assign Student Housing' | 'Assign Bill' | 'Update Bill Status' | 'Issue Bill Refund'
 
+/*
+	TO DO: Insert in other services based on different action type
+	Remaining:
+	'Application Status' | 'Bill Status' | 'Auth Register' | 'Auth Logic' | 'Change Auth Password' | 'Delete Account'
+| 	'Update User Role' | 'Submit Application' | 'Update Application Status' | 'Withdraw Application' | 'Create Housing' | 'Update Housing' |
+	'Assign Student Housing' | 'Assign Bill' | 'Update Bill Status' | 'Issue Bill Refund'
+
+*/
 export interface AuditLog{
     audit_id?: number
     timestamp: string
@@ -15,36 +23,36 @@ export interface AuditLog{
 }
 
 // CREATE AUDIT LOG
-async function createAuditLog(audit_log: AuditLog) {
+async function create(audit_log: AuditLog) {
     const { data, error } = await supabase.from('audit_log').insert([audit_log]).select();
     if (error) throw error
     return data
 } 
 
 // READ ALL AUDIT LOGS
-async function getAllAuditLogs(){
+async function getAll(){
     const { data, error } = await supabase.from('audit_log').select('*');
     if (error) throw error
     return data
 }
 
 // READ AUDIT LOGS BASED ON ACCOUNT NUMBER
-async function getAuditLogByAccountNumber(account_number: number){
+async function getByAccountNumber(account_number: number){
     const { data, error } = await supabase.from('audit_log').select('*').eq('account_number', account_number);
     if (error) throw error
     return data
 }
 
 // UPDATE AUDIT LOGS
-async function updateAuditLog(audit_id: number, updatedFields: Partial<AuditLog>){
+async function update(audit_id: number, updatedFields: Partial<AuditLog>){
     const { data, error } = await supabase.from('audit_log').update(updatedFields).eq('audit_id', audit_id).select();
     if (error) throw error
     return data
 }
 
 export const auditLogData = {
-	createAuditLog,
-	getAllAuditLogs,
-	getAuditLogByAccountNumber,
-	updateAuditLog
+	create,
+	getAll,
+	getByAccountNumber,
+	update
 }
