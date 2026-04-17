a7cc532a66f5644f3c586238e9fcfe9dad3a28b7
GiovannApolinar <gnapolinar1@up.edu.ph>
2026-04-18 00:08:32 +0800

    feat: add type for update manager profile

--- src/app/lib/models/manager.ts
+++ src/app/lib/models/manager.ts
@@ -4,16 +4,23 @@ import type {
 	TablesUpdate,
 } from "@/app/types/database.types";
 
-import { User } from "@/models/user";
+import { NewUser, User } from "@/models/user";
 
 export type Manager = Tables<"manager">;
 export type NewManager = TablesInsert<"manager">;
 export type UpdateManager = TablesUpdate<"manager">;
 
 export type ManagerPaymentDetails = Tables<"manager_payment_details">;
+export type NewManagerPaymentDetails = TablesUpdate<"manager_payment_details">;
 
 export type ManagerProfile = Omit<User, "password" | "is_deleted"> & {
 	manager: (Manager & {
 		manager_payment_details: ManagerPaymentDetails[];
 	})[];
+};
+
+export type NewManagerProfile = Omit<NewUser, "password" | "is_deleted"> & {
+	manager: NewManager & {
+		manager_payment_details: NewManagerPaymentDetails;
+	};
 };
