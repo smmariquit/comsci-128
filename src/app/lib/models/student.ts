607b6d76459279589b7d805f53d2020da1f14209
GiovannApolinar <gnapolinar1@up.edu.ph>
2026-04-17 20:29:06 +0800

    feat: add type for student profile

--- src/app/lib/models/student.ts
+++ src/app/lib/models/student.ts
@@ -1,9 +1,17 @@
 import type {
-  Tables,
-  TablesInsert,
-  TablesUpdate,
+	Tables,
+	TablesInsert,
+	TablesUpdate,
 } from "@/app/types/database.types";
+import { User } from "@/models/user";
+import { StudentAcademic } from "@/models/student_academic";
 
 export type Student = Tables<"student">;
 export type NewStudent = TablesInsert<"student">;
 export type UpdateStudent = TablesUpdate<"student">;
+
+export type StudentProfile = Omit<User, "password" | "is_deleted"> & {
+	student: (Student & {
+		student_academic: StudentAcademic[];
+	})[];
+};
