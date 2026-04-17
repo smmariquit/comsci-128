726f55b1b2a246e691cd112bf4a0770306a40e30
GiovannApolinar <gnapolinar1@up.edu.ph>
2026-04-17 23:46:29 +0800

    feat: api update for student profile

--- src/app/lib/services/profile-service.ts
+++ src/app/lib/services/profile-service.ts
@@ -1,7 +1,8 @@
 import { studentData } from "@/data/student-data";
-import { StudentProfile } from "@/models/student";
+import { NewStudentProfile, StudentProfile } from "@/models/student";
 import { ManagerProfile } from "../models/manager";
 import { managerData } from "../data/manager-data";
+import { userData } from "../data/user-data";
 
 async function getStudentProfile(
 	userId: number,
@@ -31,9 +32,45 @@ async function getManagerProfile(
 	}
 }
 
-async function updateStudentProfile(profileData: unknown) {
-	// TODO: implement updating the student profile
-	throw new Error("updateStudentProfile not implemented");
+async function updateStudentProfile(
+	userId: number,
+	profileData: NewStudentProfile,
+): Promise<StudentProfile | null> {
+	try {
+		const {
+			student,
+			account_number,
+			account_email,
+			user_type,
+			...userUpdates
+		} = profileData;
+
+		const updatedUser = await userData.updateUser(userId, userUpdates);
+		if (!updatedUser) return null;
+
+		console.log("Student data received:", student);
+		if (student) {
+			const { account_number, student_academic, ...studentDetails } =
+				student;
+
+			await studentData.updateStudent(userId, studentDetails);
+			if (student_academic) {
+				if (student_academic) {
+					const { account_number, ...studentAcademicDetails } =
+						student_academic;
+					await studentData.updateStudentAcademic(
+						userId,
+						studentAcademicDetails,
+					);
+				}
+			}
+		}
+
+		return await getStudentProfile(userId);
+	} catch (error: any) {
+		console.error("Error updating profile:", error.message);
+		throw new Error("Failed to update profile");
+	}
 }
 
 async function updateManagerProfile(profileData: unknown) {
