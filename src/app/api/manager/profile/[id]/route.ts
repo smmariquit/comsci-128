d425d51173f06accde4a799fe57f3c9c679505a9
GiovannApolinar <gnapolinar1@up.edu.ph>
2026-04-18 00:08:47 +0800

    feat: api update for manager profile

--- src/app/api/manager/profile/[id]/route.ts
+++ src/app/api/manager/profile/[id]/route.ts
@@ -33,5 +33,30 @@ export async function PATCH(
 	request: NextRequest,
 	{ params }: { params: Promise<{ id: string }> },
 ) {
-	// update profile
+	try {
+		const body = await request.json();
+		const { id } = await params;
+		const updated = await profileAction.updateManagerProfile(
+			Number(id),
+			body,
+		);
+
+		// Send Response
+		if (!updated) {
+			// Student not found
+			return NextResponse.json(
+				{ message: "manager not found." },
+				{ status: 404 },
+			);
+		}
+
+		// Student found
+		return NextResponse.json(updated, { status: 200 });
+	} catch (error: any) {
+		console.error("Error updating manager profile:", error);
+		return NextResponse.json(
+			{ message: "Failed to update manager.", error: error.message },
+			{ status: 500 },
+		);
+	}
 }
