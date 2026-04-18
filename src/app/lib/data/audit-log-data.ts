c08e236f59047bb8856dffae91b0db8c55b53c50
Zel <cysilerio@up.edu.ph>
2026-04-18 14:09:31 +0800

    refactor(audit-log): remove unnecessary variables

--- src/app/lib/data/audit-log-data.ts
+++ src/app/lib/data/audit-log-data.ts
@@ -1,17 +1,5 @@
 import { supabase } from "@/app/lib/supabase";
 
-type ActionType = "Application Status" | "Bill Status";
-
-interface AuditLog {
-	audit_id?: number;
-	timestamp: string;
-	action_type: ActionType;
-	audit_description: string;
-	user_id: number;
-	user_name: string;
-	account_number: number;
-}
-
 async function create(audit_log: AuditLog) {
 	const { data, error } = await supabase
 		.from("audit_log")
@@ -32,20 +20,6 @@ async function getByAccountNumber(account_number: number) {
 		.from("audit_log")
 		.select("*")
 		.eq("account_number", account_number);
-	if (error) throw error;
-	return data;
-}
-
-// UPDATE AUDIT LOGS
-async function updateAuditLog(
-	audit_id: number,
-	updatedFields: Partial<AuditLog>,
-) {
-	const { data, error } = await supabase
-		.from("audit_log")
-		.update(updatedFields)
-		.eq("audit_id", audit_id)
-		.select();
 	if (error) throw error;
 	return data;
 }
