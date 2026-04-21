83b07f0b2f4f2f93bcf34aa64ebde3e3c32c1913
smmariquit <smmariquit@up.edu.ph>
2026-04-21 23:07:57 +0800

    feat: fix build and typescript errors

--- src/app/api/landlord/[accountNumber]/tenants/route.ts
+++ src/app/api/landlord/[accountNumber]/tenants/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { landlordService } from "@/services/landlord";
+import { landlordService } from "@/app/lib/services/landlord-service";
 
 export async function GET(
 	_req: NextRequest,
