5323269d0ee3f2ad708588616e179d195b45d03c
Zel Silerio <crizzelesilerio@gmail.com>
2026-04-21 15:13:53 +0800

    feat(manager): add query to get all bills made by a manager

--- src/app/lib/data/manager-data.ts
+++ src/app/lib/data/manager-data.ts
@@ -456,6 +456,48 @@ const getStudentBalance = async (student_account_number: number) => {
   };
 };
 
+const getAllBillings = async () => {
+  const { data, error } = await supabase
+    .from('bill')
+    .select(`
+      transaction_id,
+      amount,
+      status,
+      due_date,
+      is_deleted,
+      student:student_account_number (
+        account_number,
+        user:account_number (
+          first_name,
+          last_name,
+          account_email
+        ),
+        student_accommodation_history (
+          room:room_id (
+            room_id,
+            housing:housing_id (
+              housing_name,
+              manager:manager_account_number (
+                account_number,
+                user:account_number (
+                  first_name, 
+                  last_name
+                )
+              )
+            )
+          )
+        )
+      )
+    `)
+    .eq('is_deleted', false)
+    .order('due_date', { ascending: false });
+
+  if (error) throw error;
+  
+
+  return data;
+};
+
 export const managerData = {
 	create,
 	getAll,
@@ -477,5 +519,6 @@ export const managerData = {
   getOccupancyRateOfHousing,
   getManagedHousings,
   getAllTenants,
-  getStudentBalance
+  getStudentBalance,
+  getAllBillings
 }
