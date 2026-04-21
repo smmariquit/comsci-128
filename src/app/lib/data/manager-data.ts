459fe6a7a651523e04280d876742866071e9d029
Zel Silerio <crizzelesilerio@gmail.com>
2026-04-21 15:13:04 +0800

    feat(manager): add query to get student balance

--- src/app/lib/data/manager-data.ts
+++ src/app/lib/data/manager-data.ts
@@ -425,6 +425,37 @@ const getAllTenants = async (managerAccountNumber: number) => {
   return data;
 };
 
+const getStudentBalance = async (student_account_number: number) => {
+  const { data, error } = await supabase
+    .from('bill')
+    .select(`
+      transaction_id,
+      amount, 
+      status,
+      student:student_account_number (
+        user:account_number (first_name, last_name)
+      ),
+      manager:manager_account_number (
+        user:account_number (last_name)
+      )
+    `)
+    .eq('student_account_number', student_account_number)
+    .eq('is_deleted', false)
+    .in('status', ['Pending', 'Overdue']);
+
+  if (error) throw error;
+
+  const total = data?.reduce((sum, bill) => {
+    return sum + Number(bill.amount);
+  }, 0);
+
+  return {
+    student: data?.[0]?.student || null,
+    totalBalance: total ?? 0,
+    bills: data
+  };
+};
+
 export const managerData = {
 	create,
 	getAll,
@@ -445,5 +476,6 @@ export const managerData = {
 	getOverallOccupancyRate,
   getOccupancyRateOfHousing,
   getManagedHousings,
-  getAllTenants
+  getAllTenants,
+  getStudentBalance
 }
