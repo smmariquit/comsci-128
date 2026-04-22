85246a0226e64f8e9b8b4fbcc7d9a8968adee8f4
GiovannApolinar <183715638+GiovannApolinar@users.noreply.github.com>
2026-04-22 14:08:44 +0000

    chore: auto-fix linting issues with Biome

--- src/app/(auth)/api/route.ts
+++ src/app/(auth)/api/route.ts
@@ -1,21 +1,21 @@
-import { NextRequest, NextResponse } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 import { userService } from "@/app/lib/services/user-service";
 
 export async function POST(request: NextRequest) {
-    try {
-        const body = await request.json();
+  try {
+    const body = await request.json();
 
-        const newStudent = await userService.addUser(body);
+    const newStudent = await userService.addUser(body);
 
-        return NextResponse.json(  
-            { message: "User registered successfully.", user: newStudent },
-            { status: 201 }
-        );
-    } catch (error: any) {
-        console.error("Error during registration:", error);
-        return NextResponse.json(
-            { message: error.message ||  "An error occurred during registration." },
-            { status: 500 }
-        );
-    }
+    return NextResponse.json(
+      { message: "User registered successfully.", user: newStudent },
+      { status: 201 },
+    );
+  } catch (error: any) {
+    console.error("Error during registration:", error);
+    return NextResponse.json(
+      { message: error.message || "An error occurred during registration." },
+      { status: 500 },
+    );
+  }
 }
