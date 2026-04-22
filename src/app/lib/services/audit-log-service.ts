9d90eb0c4c7e664799131fe93bc48400070561b9
Robin Drey Rebugio <rcrebugio@up.edu.ph>
2026-04-22 22:09:04 +0800

    feat: create audit log service

--- src/app/lib/services/audit-log-service.ts
+++ src/app/lib/services/audit-log-service.ts
@@ -1,5 +1,5 @@
 import { auditLogData } from "../data/audit-log-data";
-import { AuditLog, Role } from "@/models/audit_log";
+import { AuditLog, NewAuditLog, Role } from "@/models/audit_log";
 
 const getAllAuditLogs = async (role?: Role, account_number?: number): Promise<AuditLog[]> => {
     try {
@@ -11,8 +11,32 @@ const getAllAuditLogs = async (role?: Role, account_number?: number): Promise<Au
     }
 };
 
+const createAuditLog = async (auditLog: NewAuditLog): Promise<void> => {
+    try {
+        // Create Audit Log 
+        const auditlog = await auditLogData.create({
+            action_type: auditLog.action_type,
+            audit_description: auditLog.audit_description,
+            user_name: auditLog.user_name || null,
+            partial_ip: auditLog.partial_ip,
+            account_number: auditLog.account_number || null,
+            assigned_manager: auditLog.assigned_manager || null,
+            timestamp: auditLog.timestamp
+        });
+
+        if (!auditlog) {
+            throw new Error("Failed to create audit log");
+        }
+
+    } catch (error) {
+        console.error("Error creating audit log:", error);
+        throw new Error("Failed to create audit log");
+    }
+};
+
 export const auditLogService = {
-    getAllAuditLogs
+    getAllAuditLogs,
+    createAuditLog
 }
 
 export const randomIpAddress = () => {
