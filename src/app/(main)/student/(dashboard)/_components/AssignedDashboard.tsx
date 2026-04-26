5bd9b0d33d0d696a11a27b2ecc24e57c4e2bd2e1
Jelen Corpuz <jtcorpuz5@up.edu.ph>
2026-04-26 20:31:42 +0800

    fix(ui): change all screens to sm

--- src/app/(main)/student/(dashboard)/_components/AssignedDashboard.tsx
+++ src/app/(main)/student/(dashboard)/_components/AssignedDashboard.tsx
@@ -14,16 +14,16 @@ export default function AssignedDashboard(userName: String, dashboardData: Objec
 
     return (
         <div className="flex w-full flex-col items-start justify-start gap-4 lg:max-w-[864px]">
-            <div className="inline-flex min-h-[36px] w-full items-center justify-start gap-2.5 overflow-hidden rounded-full bg-gray-800 px-6 md:px-9 py-2 shadow-md">
+            <div className="inline-flex min-h-[36px] w-full items-center justify-start gap-2.5 overflow-hidden rounded-full bg-gray-800 px-6 sm:px-9 py-2 shadow-md">
                 <div className={hStyle}>Hello, {userName}!</div>
             </div>
             
             <div className="flex w-full flex-col items-start justify-start overflow-hidden rounded-2xl bg-stone-200 shadow-md">
-                <div className="inline-flex min-h-[36px] w-full items-center justify-start gap-2.5 bg-gray-800 px-6 md:px-9 py-2">
+                <div className="inline-flex min-h-[36px] w-full items-center justify-start gap-2.5 bg-gray-800 px-6 sm:px-9 py-2">
                     <div className={hStyle}>Housing Information</div>
                 </div>
 
-                <div className="w-full flex-1 px-6 py-5 md:px-9 md:py-6">
+                <div className="w-full flex-1 px-6 py-5 sm:px-9 sm:py-6">
                     <span className={`${tStyle} font-semibold`}>Housing Details</span>
                     <ul className={`${tStyle} pb-4 px-4`}>
                         <li><span className="font-bold">Name:</span>{getHousing()?.housing_name}</li>
@@ -39,10 +39,10 @@ export default function AssignedDashboard(userName: String, dashboardData: Objec
             </div>
 
             <div className="flex w-full flex-col items-start justify-start overflow-hidden rounded-2xl bg-stone-200 shadow-md">
-                <div className="inline-flex min-h-[36px] w-full items-center justify-start gap-2.5 bg-gray-800 px-6 md:px-9 py-2">
+                <div className="inline-flex min-h-[36px] w-full items-center justify-start gap-2.5 bg-gray-800 px-6 sm:px-9 py-2">
                     <div className={hStyle}>Billing Status</div>
                 </div>
-                <div className="w-full flex-1 px-6 py-10 md:px-9"/>
+                <div className="w-full flex-1 px-6 py-10 sm:px-9"/>
             </div>
         </div>
     );
