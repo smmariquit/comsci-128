68565e04776a1399f296f62ff8eeb545cc039d7b
PaulFababeir <pffababeir@up.edu.ph>
2026-04-28 12:19:42 +0800

    chore: /admin-billing metadata description

--- src/app/(main)/admin/billing/layout.tsx
+++ src/app/(main)/admin/billing/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Billing",
+  description: "View, manage, and issue billings for users under managed properties.",
 };
 
 export default function BillingLayout({
