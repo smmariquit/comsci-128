79a41af9afeb2c66af084b44371c842884966143
GiovannApolinar <gnapolinar1@up.edu.ph>
2026-04-28 21:04:23 +0800

    feat: refactor addStudent to create user before student

--- src/app/lib/services/student-service.ts
+++ src/app/lib/services/student-service.ts
@@ -2,6 +2,7 @@ import { NewUser, User } from "../models/user";
 import { NewStudent } from "../models/student";
 import { NewStudentAcademic } from "@/models/student_academic";
 import { studentData } from "../data/student-data";
+import { userData } from "../data/user-data";
 
 export const addStudent = async (
     userDetails: NewUser,
@@ -9,7 +10,10 @@ export const addStudent = async (
     studentAcademicDetails: NewStudentAcademic,
 ): Promise<any> => {
     try {
-        const student = await studentData.create(userDetails, studentDetails, studentAcademicDetails);
+        const createdUser = await userData.create(userDetails);
+        const accountNumber = createdUser.account_number;
+
+        const student = await studentData.create(accountNumber, studentDetails, studentAcademicDetails);
         return student;
     } catch (error) {
         console.error("Error creating student:", error);
