b1f3e231eb268d13f0619750e4f2d927a6093311
yhannis4 <yoprudencio@up.edu.ph>
2026-04-30 03:19:56 +0800

    feat: working filter for accommodations

--- src/app/(main)/manage/accommodations/page.tsx
+++ src/app/(main)/manage/accommodations/page.tsx
@@ -1,12 +1,21 @@
 import type { Metadata } from "next";
 import { housingService } from "@/app/lib/services/housing-service";
 import AccommodationsPage from "./AccommodationsPage";
+import { getManagerAccountNumber } from "@/app/lib/auth";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Accommodations",
 };
 
 export default async function Page() {
-  const housings = await housingService.getAllHousingWithRooms();
+
+  const managerAccountNumber = await getManagerAccountNumber();
+
+  if(!managerAccountNumber){
+    redirect("/unauthorized");
+  }
+
+  const housings = await housingService.getAllHousingWithRoomsByManager(managerAccountNumber);
   return <AccommodationsPage housings={housings ?? []} />;
 }
