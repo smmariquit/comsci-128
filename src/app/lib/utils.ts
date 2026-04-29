4fef37ff91555151c36ea01a625d04e7e1034e32
smmariquit <smmariquit@up.edu.ph>
2026-04-30 00:23:54 +0800

    feat: landing page says continue if logged in

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -8,6 +8,18 @@ export function setCookie (name: string, value: string, days: number): void {
     document.cookie = name + "=" + value + expires + "; path=/";
 }
 
+export function getCookie (name: string): string | null {
+    if (typeof document === 'undefined') return null;
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
 export function deleteCookie (name: string): void {
     setCookie(name, "", -1);
 }
