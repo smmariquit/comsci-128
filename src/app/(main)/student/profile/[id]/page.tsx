349a48eab061a6b7ab8e08d0207c45657bd5ed84
GiovannApolinar <gnapolinar1@up.edu.ph>
2026-05-09 14:08:19 +0800

    feat: working logout function for student dashboard

--- src/app/(main)/student/profile/[id]/page.tsx
+++ src/app/(main)/student/profile/[id]/page.tsx
@@ -6,6 +6,7 @@ import { StudentProfile } from "@/app/lib/models/student";
 import { LogOut } from "lucide-react";
 import LogoutModal from "../../../../components/LogoutModal";
 import StateMessage from "@/app/components/ui/state-message";
+import { deleteCookie } from "@/app/lib/utils";
 
 type StudentPayload = Omit<StudentProfile, "student"> & {
 	student:
@@ -385,7 +386,12 @@ export default function StudentProfilePage() {
 			onClose={() => setShowLogoutModal(false)}
 			onConfirm={() => {
 				setShowLogoutModal(false);
-				console.log("Confirmed logout"); // place logout backend code here
+				document.cookie.split(";").forEach((cookie) => {
+					const eqPos = cookie.indexOf("=");
+					const name = (eqPos > -1 ? cookie.slice(0, eqPos) : cookie).trim();
+					deleteCookie(name);
+				});
+				window.location.href = "/";
 			}}
 		/>
 
