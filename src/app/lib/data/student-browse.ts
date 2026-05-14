a95282011c01dd5ead8d42045d2bc5d7e620ca49
pajamolod <pajamolod@up.edu.ph>
2026-05-14 15:34:22 +0800

    feat: added the room type (sex) filtering

--- src/app/lib/data/student-browse.ts
+++ src/app/lib/data/student-browse.ts
@@ -2,19 +2,24 @@ import { supabase } from "@/app/lib/supabase";
 
 interface FilterOptions {
   housing_type?: "UP Housing" | "Non-UP Housing";
-  // TODO: filtering by sex (not yet implemented in the database)
-  // sex?: "Male" | "Female" | "Co-ed";
+  sex?: "Men Only" | "Women Only" | "Co-ed";
   sort_by_price?: "asc" | "desc";
   search?: string;
 }
 
 export async function getAllAvailableDorms(filters?: FilterOptions) {
   try {
-    const { data: rooms, error: roomError } = await supabase
+    let roomQuery = supabase
       .from("room")
-      .select("housing_id")
+      .select("housing_id, room_type")
       .eq("is_deleted", false)
       .not("occupancy_status", "eq", "Fully Occupied");
+
+    if (filters?.sex){
+        roomQuery = roomQuery.eq("room_type", filters.sex);
+    }
+    
+    const { data: rooms, error: roomError } = await roomQuery;
 
     if (roomError) throw roomError;
 
@@ -46,12 +51,6 @@ export async function getAllAvailableDorms(filters?: FilterOptions) {
         (dorm) => dorm.housing_type === filters.housing_type,
       );
     }
-
-    // if (filters?.sex){
-    //     filteredDorms = filteredDorms.filter(
-    //         (dorm) => dorm.sex == filters.sex
-    //     );
-    // }
 
     if (filters?.sort_by_price) {
       filteredDorms.sort((a, b) => {
