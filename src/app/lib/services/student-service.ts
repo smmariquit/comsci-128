733636640f20bd5eefada69f4092835ee11c4d2e
Robin Drey Rebugio <rcrebugio@up.edu.ph>
2026-05-14 17:40:06 +0800

    feat: student serivce dactivate

--- src/app/lib/services/student-service.ts
+++ src/app/lib/services/student-service.ts
@@ -2,6 +2,7 @@ import { NewUser, User } from "../models/user";
 import { NewStudent } from "../models/student";
 import { NewStudentAcademic } from "@/models/student_academic";
 import { studentData } from "../data/student-data";
+import { Student } from "../models/student";
 
 export const addStudent = async (
     userDetails: NewUser,
@@ -15,4 +16,19 @@ export const addStudent = async (
         console.error("Error creating student:", error);
         throw error;
     }
+};
+
+const deactivateStudent = async (accountNumber: number): Promise<Student | null> => {
+  try {
+    const student = await studentData.deactivate(accountNumber);
+    if (!student) return null;
+    return student;
+  } catch (error) {
+    console.error("Error deactivating student:", error);
+    throw new Error("Failed to deactivate student");
+  }
+};
+
+export const studentService = {
+  deactivateStudent, 
 };
