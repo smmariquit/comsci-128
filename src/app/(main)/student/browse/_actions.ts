5d0ff69e38c1beea0901bc8a4d35aba14de67a6d
smmariquit <smmariquit@up.edu.ph>
2026-05-18 07:52:28 +0800

    fix: pass actual database rooms to 3D viewer instead of mock generated array

--- src/app/(main)/student/browse/_actions.ts
+++ src/app/(main)/student/browse/_actions.ts
@@ -1,6 +1,6 @@
 import { housingData } from "@/app/lib/data/housing-data";
 
 export async function getDormDetails(id: number) {
-  const dorm = await housingData.findById(id);
+  const dorm = await housingData.findWithRooms(id);
   return dorm;
 }
