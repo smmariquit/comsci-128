d1d4495c37079c8e35cebc5eea305f39b8c0225c
smmariquit <smmariquit@up.edu.ph>
2026-05-19 10:27:53 +0800

    Clean branch: retain only the load testing script

--- src/app/(main)/student/browse/_actions.ts
+++
@@ -1,6 +0,0 @@
-import { housingData } from "@/app/lib/data/housing-data";
-
-export async function getDormDetails(id: number) {
-    const dorm = await housingData.findById(id);
-    return dorm;
-}
--- src/app/lib/models/application.ts
+++
@@ -1,5 +0,0 @@
-import { Tables, TablesInsert, TablesUpdate } from "@/app/types/database.types";
-
-export type Application = Tables<"application">;
-export type NewApplication = TablesInsert<"application">;
-export type UpdateApplication = TablesUpdate<"application">;
