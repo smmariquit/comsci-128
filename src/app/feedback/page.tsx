bcdf4dd78bea5e69112135aeda27169be7278ddc
Zel Silerio <crizzelesilerio@gmail.com>
2026-05-19 10:17:24 +0800

    feat: add page for feedback screen

---
+++ src/app/feedback/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import FeedbackForm from "@/app/components/FeedbackForm";
+
+export default function FeedbackPage() {
+  return (
+    <div className="min-h-screen py-10 bg-gray-950">
+      <div className="max-w-4xl mx-auto px-4">
+        <FeedbackForm />
+      </div>
+    </div>
+  );
+}
